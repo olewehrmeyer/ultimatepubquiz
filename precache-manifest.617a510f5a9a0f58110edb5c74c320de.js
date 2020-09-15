27f08322b23a6e343158649a7b62adc68e434c9c
Ole Wehrmeyer <ole@wehrmeyer.me>
2020-09-15 22:51:34 +0200

    Updates

---
+++ precache-manifest.617a510f5a9a0f58110edb5c74c320de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d58fe33c643f8709431addab82661ac",
+    "url": "/ultimatepubquiz/index.html"
+  },
+  {
+    "revision": "545a5e7a1689fdd24d81",
+    "url": "/ultimatepubquiz/static/css/main.9c3cbadc.chunk.css"
+  },
+  {
+    "revision": "f4564c05c32e5f3022c2",
+    "url": "/ultimatepubquiz/static/js/2.ab8d5d5a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ultimatepubquiz/static/js/2.ab8d5d5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545a5e7a1689fdd24d81",
+    "url": "/ultimatepubquiz/static/js/main.c326e723.chunk.js"
+  },
+  {
+    "revision": "0627436a5968d051d9cb",
+    "url": "/ultimatepubquiz/static/js/runtime-main.d740d96e.js"
+  }
+]);
