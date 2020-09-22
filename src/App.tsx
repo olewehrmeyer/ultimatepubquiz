f9dbe2785a1eb62312c1bae472e1f688064e9825
Ole Wehrmeyer <ole.wehrmeyer@btc-ag.com>
2020-09-22 15:27:45 +0200

    Add more taglines

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,14 @@ const subtitles = [
   "Sogar von Agile Coaches bedienbar! :-P",
   "Für jede falsche Antwort trinkt Wiebke einen Kurzen!",
   "Findet ihr Timeboxes immer noch super, wenn ihr die Fragen beantworten müsst?",
-  "Pro-Tipp: Drücke Alt+F4, um den Text der aktuellen Frage bei Google zu suchen"
+  "Pro-Tipp: Drücke Alt+F4, um den Text der aktuellen Frage bei Google zu suchen",
+  "90/10 Regel im PM: 90% der Arbeit mit 10% des Budgets",
+  "Ist nicht schlimm, dass wir keinen Plan haben, wir sind ja agil!",
+  "Unabhängig von dem, was der Punktestand sagt, glauben wir, dass jeder sein oder ihr Bestes gegeben hat, unter Berücksichtigung des Timings, seiner oder ihrer Fähigkeiten und Kompetenzen, der zur Verfügung stehenden Mittel und der Situation",
+  "Das dauert schon viel zu lange und du brauchst dringend einen Drink? Willkommen im 'Programmierer im Meeting'-Simulator 2020!",
+  "Agile Coaches dürfen bei Schätzfragen nur mit Zahlen der Fibonacci-Reihe antworten",
+  "Klassische Projektmanager sollten bei Schätzfragen ihren Wert vor dem Absenden verdoppelt, um die Trefferwahrscheinlichkeit zu erhöhen",
+  "Testabdeckung ist wie Alkohol: Je mehr, desto besser, aber irgendwann ist es einfach zum Kotzen"
 ];
 
 const MainGrid = styled.div`
